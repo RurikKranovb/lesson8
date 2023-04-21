57ae2cbcb3b0177f85084e8b055da15dae1a138a
bobrov.da <dmr.bobrov@yandex.ru>
2023-04-22 00:19:53 +0300

    Add Task54 & Task56

---
+++ Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task54
+{
+
+    //https://metanit.com/sharp/tutorial/2.7.php
+    //Программа сортировки массива
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int nextItem = 1;
+            Console.WriteLine("Введите размеры массива");
+            var m = Convert.ToInt32(Console.ReadLine());
+            var n = Convert.ToInt32(Console.ReadLine());
+            var array = new int[m, n];
+
+            CreateArray(array);
+            ArrayOutputConsole(array);
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(1) - 1; k++)
+                    {
+                        if (array[i, k] < array[i, k + nextItem])
+                        {
+                            int temp = array[i, k + nextItem];
+                            array[i, k + nextItem] = array[i, k];
+                            array[i, k] = temp;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Массив после сортировки:");
+            ArrayOutputConsole(array);
+
+        }
+
+
+
+        private static void ArrayOutputConsole(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write(array[i, j] + "\t  ");
+                Console.WriteLine();
+
+            }
+        }
+
+        private static void CreateArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    array[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            }
+        }
+    }
+}
+
+
+
---
+++ Task56/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Наш массив:");
+            var m = 4;
+            var n = 4;
+            var array = new int[m, n];
+
+            CreateArray(array);
+            ArrayOutputConsole(array);
+
+            var sumItems = 0;
+            var line = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                var item = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    item += array[i, j];
+
+                    if (sumItems >= item) continue;
+                    line = i;
+                    sumItems = item;
+
+                }
+            }
+
+            Console.WriteLine("Строка с наименьшей суммов:");
+            Console.WriteLine(line);
+        }
+
+        private static void ArrayOutputConsole(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write(array[i, j] + "\t  ");
+                Console.WriteLine();
+
+            }
+        }
+
+        private static void CreateArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    array[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+            }
+        }
+
+    }
+}
